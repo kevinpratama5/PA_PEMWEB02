28309112e608039c237f22df508211cf07434548
kevinpratama5 <54745361+kevinpratama5@users.noreply.github.com>
2024-11-08 17:51:14 +0800

    Delete PA_PEMWEB002/PA_PEMWEB02 directory

--- PA_PEMWEB002/PA_PEMWEB02/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Menghapus semua variabel session
-session_destroy(); // Menghancurkan session
-
-header("Location: index.php"); // Arahkan kembali ke beranda setelah logout
-exit();
-?>
